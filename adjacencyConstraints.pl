cb78d154e4885a0b2cd9976ffdd98396addfb6b0
ManfredLeka <manfred.leka@gmail.com>
2016-03-11 15:25:13 +0100

    adjacency constraints workiiiiiing, non overlapping to debug git add *

--- adjacencyConstraints.pl
+++ adjacencyConstraints.pl
@@ -2,10 +2,12 @@
 % adjacency constraint btween two rectangles with an intersection of at least 1 meter width for a door
 
 % collect and post the adjacency constraints 
+postAdjacencyConstraints([] , _).
+
 postAdjacencyConstraints([AdjRoomName |AdjRoomNames], SpaceVarList):-
 	postAdjacencyConstraints(AdjRoomNames, SpaceVarList),
-	adj(AdjRoomName, AdjList)),
-	getSpaceVarFromName(AdjRoomName, AdjRoomSpaceVar),
+	adj(AdjRoomName, AdjList),
+	getSpaceVarFromName(AdjRoomName, SpaceVarList, AdjRoomSpaceVar),
 	getCoordinates(AdjRoomSpaceVar, AdjRoomCoord),
 	getAdjacencyConstraint(AdjRoomCoord, AdjList, SpaceVarList, A),
 	call(A).
@@ -13,20 +15,20 @@ postAdjacencyConstraints([AdjRoomName |AdjRoomNames], SpaceVarList):-
 
 getAdjacencyConstraint(_, [], _, _).
 
-getAdjacencyConstraints(AdjRoomCoord, [Room1Name | RoomNames], SpaceVarList, A):-
-	getAdjacencyConstraints(AdjRoomCoord, RoomNames,SpaceVarList, C),
+getAdjacencyConstraint(AdjRoomCoord, [Room1Name | RoomNames], SpaceVarList, A):-
+	getAdjacencyConstraint(AdjRoomCoord, RoomNames,SpaceVarList, C),
 	getSpaceVarFromName(Room1Name, SpaceVarList, Room1SpaceVar),
 	getCoordinates(Room1SpaceVar, Room1Coord),
-	adjacencyContraint(AdjRoomCoord, Room2Coord, B),
+	adjacencyConstraint(AdjRoomCoord, Room2Coord, B),
 	(nonvar(C) -> 
-		A = B #/\ C;
+		A = (B #/\ C);
 		A = B
 	).	
 
 % adjacency constraint on two rectangular rooms
 adjacencyConstraint([X1, H1, Y1, V1], [X2, H2, Y2, V2], A):-
-	A = (((X2 #>= X1) #/\ (X2 + 1 #=< X1 + H1) #/\ ((Y2 #= Y1 + V1 ) #\ (Y2 + V2 #= Y1)))
-			#\ ((Y2 #>= Y1) #/\ (Y2 + 1 #=< Y1 + V1) #/\ ((X2 + H2 #= X1) #\ (X2 #= X1 + H1)))).
+	A = (((X2 #>= X1) #/\ (X2 + 1 #=< X1 + H1) #/\ ((Y2 #= Y1 + V1 ) #\/ (Y2 + V2 #= Y1)))
+			#\/ ((Y2 #>= Y1) #/\ (Y2 + 1 #=< Y1 + V1) #/\ ((X2 + H2 #= X1) #\/ (X2 #= X1 + H1)))).
 
 
 % OR distribution of adjacency constraint for L shaped rooms
@@ -39,6 +41,6 @@ adjacencyConstraint(Room1, [Room2, Room3], A):-
 adjacencyConstraint([Room1, Room2], Room3, A):-
 	adjacencyConstraint(Room1, Room3, B),
 	adjacencyConstraint(Room2, Room3, C),
-	A = B #\/ C.
+	A = (B #\/ C).
 
 

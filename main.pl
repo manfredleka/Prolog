d1a80f6fc4b000cccf549842e88a0aa2f1647a79
ManfredLeka <manfred.leka@gmail.com>
2016-03-25 08:56:27 +0100

    same

--- main.pl
+++ main.pl
@@ -105,7 +105,7 @@ main(Solution):-
 	flatten(AllVariables, Variables),
 	writeln('initiating labeling'),
 
-	labeling([ff], Variables),
+	labeling([ff, up, bisect], Variables),
 	%LostSpace2 = LostSpace,
 	Solution = SpaceVarList,
 	printSolution(SpaceVarList).
